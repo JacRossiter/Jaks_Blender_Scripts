ecb39fd4c653b1b37f2ea41834683fe812a70c22
Jac Rossiter <jacrossiter.art@gmail.com>
2017-09-23 16:09:51 +0100

    Added an alternate version of the Delete_Extra_UVs Scripts
    Courtesy of Martin Durhuus(Dur)

---
+++ UV Tools/Delete_Extra_UVs_Vers2.py
@@ -0,0 +1,10 @@
+import bpy
+# This script wil delete all uvmaps in the selected objects that do not have the name 'UVMap' 
+
+for obj in bpy.context.selected_objects: # Creates list of uv maps that need deleting
+    uvmaps = obj.data.uv_textures
+    while len(uvmaps) > 1:
+        if uvmaps[-1].name == 'UVMap':
+            uvmaps.remove(uvmaps[0])
+        else:
+            uvmaps.remove(uvmaps[-1])
