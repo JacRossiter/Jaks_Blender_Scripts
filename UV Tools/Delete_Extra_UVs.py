f7a76ed69c5052a588721d4a806f66b197277581
Jac Rossiter <jacrossiter.art@gmail.com>
2017-09-23 15:43:04 +0100

    Adjusted naming and added a text document to the UV Folder

--- UV Tools/Delete_Extra_UVs.py
+++ UV Tools/Delete_Extra_UVs.py
@@ -14,9 +14,6 @@ for obj in bpy.context.selected_objects:
     #for x in uv_delete:
     #    print(x)
     # Gets length of list and starts deleting in a desending order. This is because when you delete a uvmap it actually still exists but contains the name 'NGon Face'. 
-    #-This uvmap's position in the list will be shifted. This rearranges the list and causes issues for us if we try to iterate through in a acending manner.
-    x = len(uv_delete)-1
-    for uv in uv_delete:
-        #print("Trying to delete:",uv_delete[x], "from", obj.name)
-        uvtex.remove(uv_delete[x])
-        x = x-1
+    #-This uvmap's position in the list will be shifted. This rearranges the list and causes issues for us if we try to iterate through in a acending manner
+    for i in range(len(uv_delete)):
+        uv_delete.remove(uv_delete[-1])
