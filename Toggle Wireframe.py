061be3cddd82db9ca2e71b8b2395c8780058041f
JacRossiter <jacrossiter.art@gmail.com>
2017-09-23 16:31:33 +0100

    Reverted some changes

--- Toggle Wireframe.py
+++ Toggle Wireframe.py
@@ -3,10 +3,10 @@ import bpy
 ob = bpy.context.scene.objects.active
 
 if ob.show_wire == True:
-    for ob in bpy.context.selected_objects:
-        ob.show_wire = False
+    for obj in bpy.context.selected_objects:
+        obj.show_wire = False
     
 elif ob.show_wire == False:
-    for ob in bpy.context.selected_objects:
-        ob.show_wire = True
-        ob.show_all_edges = True
+    for obj in bpy.context.selected_objects:
+        obj.show_wire = True
+        obj.show_all_edges = True
