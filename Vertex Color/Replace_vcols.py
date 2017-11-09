2d87907bbbb2440ade958f38bcd585039ed65334
Jac Rossiter <jacrossiter.art@gmail.com>
2017-11-09 13:24:38 +0000

    fixed indentations

--- Vertex Color/Replace_vcols.py
+++ Vertex Color/Replace_vcols.py
@@ -12,24 +12,24 @@ def replace_vcols_object():
 
 
     # Add vcols
-        for mesh in bpy.context.selected_objects:
-            bpy.context.scene.objects.active = ob   # Sets current object as active
-            # Create Vertex Color for each selected meshes
-            # bpy.context.selected_objects[0].data.vertex_colors
-            mesh.data.vertex_colors.new()
+    for mesh in bpy.context.selected_objects:
+        bpy.context.scene.objects.active = ob   # Sets current object as active
+        # Create Vertex Color for each selected meshes
+        # bpy.context.selected_objects[0].data.vertex_colors
+        mesh.data.vertex_colors.new()
 
-        # Get Total Length of Vertex Colors for each selected meshes
-        # len(bpy.context.selected_objects[0].data.vertex_colors[0].data)
-            totalVertCol = len(mesh.data.vertex_colors[0].data)
-        
-        # Iterate over every mesh vertex color and give it a single colour
-        
-            colR = random.random()
-            colG = random.random()
-            colB = random.random()
+    # Get Total Length of Vertex Colors for each selected meshes
+    # len(bpy.context.selected_objects[0].data.vertex_colors[0].data)
+        totalVertCol = len(mesh.data.vertex_colors[0].data)
+    
+    # Iterate over every mesh vertex color and give it a single colour
+    
+        colR = random.random()
+        colG = random.random()
+        colB = random.random()
 
-            for i in range(totalVertCol):
-                mesh.data.vertex_colors[0].data[i].color = colR, colG, colB
+        for i in range(totalVertCol):
+            mesh.data.vertex_colors[0].data[i].color = colR, colG, colB
 
 def replace_vcols_edit():
     ob = bpy.context.scene.objects.active
